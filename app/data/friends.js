5a27d987e28a224cd47bc1ed411ecdde2f1a599b
gcolvin77 <Tubatica@gmail.com>
2018-07-17 17:42:17 -0600

    update files

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+var friendList = [
+
+    {
+       "name": "Donald",
+       "photo": "https://cdn.theatlantic.com/assets/media/img/mt/2016/09/RTX1GZCO/lead_720_405.jpg?mod=1522791065" ,
+       "scores": [3,3,3,3,3,3,3,3,3,3]
+    }
+
+]
+
+module.exports = friendList;
